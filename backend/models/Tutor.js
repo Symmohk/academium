1472452e1c4211f85e7f7dbba826a98564093081
Symmohk <simonkomba1@gmail.com>
2025-07-11 20:17:50 +0300

    Update Tutor.js

--- backend/models/Tutor.js
+++ backend/models/Tutor.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
 
+const TutorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  subject: { type: String, required: true },
+  curriculum: { type: String, enum: ['IGCSE', 'CBC', 'GCSE'], required: true },
+  rating: { type: Number, default: 0 },
+  sessions: { type: Number, default: 0 },
+  hourlyRate: { type: Number, required: true },
+  online: { type: Boolean, default: false },
+  image: { type: String }
+});
+
+module.exports = mongoose.model('Tutor', TutorSchema);
